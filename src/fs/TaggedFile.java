91fcfb6f2da115ed2140429c557512e3acdcfb6f
Elton Kjeld Schiott <ekschiott@gmail.com>
2017-11-03 20:13:16 -0400

    Fixed filtering

--- src/fs/TaggedFile.java
+++ src/fs/TaggedFile.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
-import java.util.ArrayList;
 import java.util.List;
 
 public class TaggedFile extends File {
